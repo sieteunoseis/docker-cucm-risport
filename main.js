ef2e5123d68649ccdb7765d95ec192d0c7da3340
Jeremy <jeremy.worden@gmail.com>
2022-10-31 10:55:59 -0700

    v1.1.1

--- main.js
+++ main.js
@@ -1,10 +1,7 @@
 const risPortService = require("cisco-risport");
 const Models = require("./js/Models");
 const StatusReason = require("./js/statusReasons");
-
 const { InfluxDB, Point } = require("@influxdata/influxdb-client");
-const sleep = (waitTimeInMs) =>
-  new Promise((resolve) => setTimeout(resolve, waitTimeInMs));
 
 if (process.env.NODE_ENV !== "production") {
   require("dotenv").config();
@@ -38,68 +35,62 @@ setInterval(function () {
   (async () => {
     const writeApi = client.getWriteApi(org, bucket);
     var points = [];
-    await sleep(timer).then(async () => {
-      var service = new risPortService(
-        process.env.CUCM_PUB,
-        process.env.CUCM_USERNAME,
-        process.env.CUCM_PASSWORD
-      );
-      var risportOutput = await service.selectCmDevice(
-        process.env.RISPORT_SOAPACTION,
-        process.env.RISPORT_MAXRETURNEDDEVICES,
-        process.env.RISPORT_DEVICECLASS,
-        process.env.RISPORT_MODEL,
-        process.env.RISPORT_STATUS,
-        process.env.RISPORT_NODE,
-        process.env.RISPORT_SELECTBY,
-        process.env.RISPORT_SELECTITEM,
-        process.env.RISPORT_PROTOCOL,
-        process.env.RISPORT_DOWNLOADSTATUS
-      );
+    var service = new risPortService(
+      process.env.CUCM_PUB,
+      process.env.CUCM_USERNAME,
+      process.env.CUCM_PASSWORD
+    );
+    var risportOutput = await service.selectCmDevice(
+      process.env.RISPORT_SOAPACTION,
+      process.env.RISPORT_MAXRETURNEDDEVICES,
+      process.env.RISPORT_DEVICECLASS,
+      process.env.RISPORT_MODEL,
+      process.env.RISPORT_STATUS,
+      process.env.RISPORT_NODE,
+      process.env.RISPORT_SELECTBY,
+      process.env.RISPORT_SELECTITEM,
+      process.env.RISPORT_PROTOCOL,
+      process.env.RISPORT_DOWNLOADSTATUS
+    );
 
-      if (Array.isArray(risportOutput)) {
-        risportOutput.map((item) => {
-          if (item.ReturnCode === "Ok") {
-            server = item.Name;
-            writeApi.useDefaultTags({ host: server });
-            item.CmDevices.item.map((item) => {
-              var d = new Date(0); // The 0 there is the key, which sets the date to the epoch
-              d.setUTCSeconds(item.TimeStamp);
+    if (Array.isArray(risportOutput)) {
+      risportOutput.map((item) => {
+        if (item.ReturnCode === "Ok") {
+          server = item.Name;
+          writeApi.useDefaultTags({ host: server });
+          item.CmDevices.item.map((item) => {
+            var d = new Date(0); // The 0 there is the key, which sets the date to the epoch
+            d.setUTCSeconds(item.TimeStamp);
 
-              points.push(
-                new Point(item.DeviceClass)
-                  .tag("ipAddress", item.IPAddress.item.IP)
-                  .tag(
-                    "statusReason",
-                    StatusReason[parseInt(item.StatusReason)]
-                  )
-                  .tag("name", item.Name)
-                  .tag("model", Models[parseInt(item.Model)])
-                  .tag("userId", item.LoginUserId)
-                  .tag("protocol", item.Protocol)
-                  .tag("activeLoad", item.ActiveLoadID)
-                  .tag("downloadStatus", item.DownloadStatus)
-                  .tag("registrationAttempts",item.RegistrationAttempts)
-                  .tag("status", item.Status)
-                  .stringField("lastRegistration",d)
+            points.push(
+              new Point(item.DeviceClass)
+                .tag("ipAddress", item.IPAddress.item.IP)
+                .tag("statusReason", StatusReason[parseInt(item.StatusReason)])
+                .tag("name", item.Name)
+                .tag("model", Models[parseInt(item.Model)])
+                .tag("userId", item.LoginUserId)
+                .tag("protocol", item.Protocol)
+                .tag("activeLoad", item.ActiveLoadID)
+                .tag("downloadStatus", item.DownloadStatus)
+                .tag("registrationAttempts", item.RegistrationAttempts)
+                .tag("status", item.Status)
+                .stringField("lastRegistration", d)
+            );
+          });
+
+          writeApi.writePoints(points);
+          writeApi
+            .close()
+            .then(() => {
+              console.log(
+                `RISPORT DATA: Wrote ${points.length} points to InfluxDB bucket ${bucket}`
               );
-
+            })
+            .catch((e) => {
+              console.log("RISPORT DATA: InfluxDB write failed", e);
             });
-
-            writeApi.writePoints(points);
-            writeApi
-              .close()
-              .then(() => {
-                console.log(
-                  `RISPORT DATA: Wrote ${points.length} points to InfluxDB bucket ${bucket}`
-                );
-              })
-              .catch((e) => {
-                console.log("RISPORT DATA: InfluxDB write failed", e);
-              });
-          }
-        });
-      }
-    });
+        }
+      });
+    }
   })();
 }, interval);
